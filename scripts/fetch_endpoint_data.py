5d41f4f73da3b093117c000fdc0904e3804437fb
Shing Hei Zhan <szhan@Shings-MacBook-Pro.local>
2017-06-18 15:42:47 -0600

    Update example workflow to get basic data about matches

--- scripts/fetch_endpoint_data.py
+++ scripts/fetch_endpoint_data.py
@@ -1,6 +1,7 @@
 import json
 import common_tools as ct
 
+
 """
 Basic workflow to get match endpoint data for 20 recent 
 	ranked solo queue matches for all Challenger players in NA.
@@ -29,14 +30,20 @@ api_cmd = ct.get_challengers_by_queue(URL_PREFIX, URL_SUFFIX, QUEUE_TYPE)
 list_challengers = ct.get_json_by_request(api_cmd)
 
 # Decode into dict, and extract player names, and wins and losses per player
-obj = json.loads(list_challengers)	# LeagueListDTO
-for entry in obj["entries"]:
+league_list_dto = json.loads(list_challengers)	# LeagueListDTO
+for league_item_dto in league_list_dto["entries"][:3]:
 	""" Iterate through list of LeagueItemDTO. """
-	print "\t".join([
-			entry["playerOrTeamName"],
-			str(entry["playerOrTeamId"]),
-			str(entry["wins"]),
-			str(entry["losses"]),
-			str(entry["leaguePoints"])
-			])
+	player_id = league_item_dto["playerOrTeamId"]
+	summoner_dto = json.loads(ct.get_json_by_request(ct.get_summoner_by_id(URL_PREFIX, URL_SUFFIX, player_id)))
+	account_id = summoner_dto["accountId"]
+	match_list_dto = json.loads(ct.get_json_by_request(ct.get_match_list_by_account_id(URL_PREFIX, URL_SUFFIX, account_id, recent=True)))
+	
+	for match_reference_dto in match_list_dto["matches"]:
+		print "\t".join([
+				str(account_id),
+				str(match_reference_dto["gameId"]),
+				str(match_reference_dto["lane"]),
+				str(match_reference_dto["champion"]),
+				str(match_reference_dto["role"])
+				])
 
